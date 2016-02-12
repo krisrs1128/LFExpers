8397e92e852a9918039762171d9041c9a7f37fc9
Kris Sankaran <sankaran.kris@gmail.com>
2016-02-12 00:05:53 -0800

    setup a simpple simulation

--- vignettes/spline_sim.R
+++ vignettes/spline_sim.R
@@ -33,3 +33,33 @@ ggplot(mHB) +
   geom_line(aes(x = time, y = value, group = source), size = .5, alpha = 0.8) +
   facet_wrap(~type) +
   ggtitle("True vs. fitted sources (unidentifiable)")
+
+## ---- simulation-data ----
+N <- 150
+P <- 20
+K <- 5
+L <- 6
+
+library("splines")
+H <- bs(1:N, df = L, degree = 1)
+W <- matrix(rnorm(P * K), P, K)
+B <- matrix(rnorm(L * K), L, K)
+B0 <- matrix(rnorm(L * K), L, K)
+W0 <- matrix(rnorm(P * K), P, K)
+
+sigmas <- 10 ^ seq(-1, 1, length.out = 10)
+na_props <- seq(0, .95, length.out = 10)
+fits <- vector(length = 100, mode = "list")
+
+ix <- 1
+for(i in seq_along(sigmas)) {
+  for(j in seq_along(na_props)) {
+    E <- matrix(sigmas[i] * rnorm(N * P), N, P)
+    Y <- H %*% B %*% t(W) + E
+    Y[sample(N * P, N * P * na_props[j])] <- NA # 40% missing at random
+
+    # fit the model
+    fits[[ix]] <- spline_lf(Y, H, B0, W0, list(n_iter = 50))
+    ix <- ix + 1
+  }
+}
