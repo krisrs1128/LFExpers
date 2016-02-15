dd85c02ae159c62ca25022e70f62a6a49fb6cb7d
Kris Sankaran <sankaran.kris@gmail.com>
2016-02-15 14:56:43 -0800

    return objective for logistic latent factors

--- R/lf_logit.R
+++ R/lf_logit.R
@@ -24,7 +24,7 @@ logit_lf <- function(Y, Phi0, W0, opts = list()) {
 
   # initialize results
   param <- list(Phi = Phi0, W = W0)
-  obj_names <- c("iter", "ll", "01", "10", "W1", "W2", "Phi1", "Phi2")
+  obj_names <- c("iter", "ll", "W1", "W2", "Phi1", "Phi2", "obj")
   obj <- matrix(NA, opts$n_iter, length(obj_names),
                 dimnames = list(1:opts$n_iter, obj_names))
 
@@ -34,13 +34,23 @@ logit_lf <- function(Y, Phi0, W0, opts = list()) {
   for(i in seq_len(opts$n_iter)) {
     param$W <- independent_models(param$Phi, Y, logit_W)
     param$Phi <- independent_models(param$W, t(Y), logit_Phi)
-    cat(sprintf("iter %f", i))
-    #obj[i, ] <- c(i, lf_logit_obj)
+    cur_obj <- lf_logit_obj(Y, param$Phi, param$W, opts)
+
+    obj[i, ] <- c(i, cur_obj)
+    cat(sprintf("iter %g | ll: %f | W1: %f | W2: %f | Phi1: %f | Phi2: %f | obj %f \n",
+                obj[i, 1], obj[i, 2], obj[i, 3], obj[i, 4],
+                obj[i, 5], obj[i, 6], obj[i, 7]))
   }
   c(param, list(obj = obj))
-
 }
 
-lf_logit_obj <- function(Y, Phi, W) {
-
+lf_logit_obj <- function(Y, Phi, W, opts) {
+  ll <- mean(Y * Phi %*% t(W) - log(1 + exp(Phi %*% t(W))))
+  W1 <- sum(abs(W))
+  W2 <- sum(W ^ 2)
+  Phi1 <- sum(abs(Phi))
+  Phi2 <- sum(Phi ^ 2)
+  obj <- ll - elnet_pen(opts$lambda_W, opts$alpha_W, W1, W2) -
+    elnet_pen(opts$lambda_Phi, opts$alpha_Phi, Phi1, Phi2)
+  c(ll, W1, W2, Phi1, Phi2, obj)
 }
